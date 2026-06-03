f4f50ec0a7084f1d8f26a3ed5631046aa81428eb
Kenil Savaliya <your-email@example.com>
2026-06-04 00:05:16 +0530

    added documentUpload functionality

--- src/main/java/com/claims/document/dto/response/DocumentResponseDTO.java
+++ src/main/java/com/claims/document/dto/response/DocumentResponseDTO.java
@@ -1,4 +1,26 @@
 package com.claims.document.dto.response;
 
+import com.claims.document.DocumentType;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class DocumentResponseDTO {
+
+    private String claimId;
+
+    private String documentId;
+
+    private DocumentType documentType;
+
+    private String originalFileName;
+
+    private String contentType;
+
+    private Long fileSize;
+
+    private LocalDateTime uploadedAt;
 }
--- src/main/java/com/claims/document/repository/DocumentRepository.java
+++ src/main/java/com/claims/document/repository/DocumentRepository.java
@@ -2,9 +2,15 @@ package com.claims.document.repository;
 
 import com.claims.document.entity.DocumentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface DocumentRepository extends JpaRepository<DocumentEntity,Long> {
+@Repository
+public interface DocumentRepository extends JpaRepository<DocumentEntity, Long> {
+
     List<DocumentEntity> findByClaimId(String claimId);
+
+    Optional<DocumentEntity> findByDocumentId(String documentId);
 }
